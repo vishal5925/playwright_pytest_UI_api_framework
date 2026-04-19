52a174d03e232c00efd11835c074f500ae40eee1
Vishal Verma <=>
2026-04-19 10:20:30 +0530

    set headless true

--- test/sample/test_locators.py
+++ test/sample/test_locators.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo= 500)
+    browser = p.chromium.launch(headless=True, slow_mo= 500)
     page= browser.new_page()
     page.goto("https://demo.playwright.dev/todomvc/#/")
